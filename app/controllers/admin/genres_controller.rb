e59248753846228dbc2be3ed7beedfcdc5630f07
EC2 Default User <ec2-user@ip-172-31-16-44.ap-northeast-1.compute.internal>
2023-10-21 09:36:00 +0000

    [Add]管理者側商品詳細画面

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,5 @@
 class Admin::GenresController < ApplicationController
+  def index
+    @genres = @item.genres.page(params[:page])
+  end
 end
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,15 +1,22 @@
 class Admin::ItemsController < ApplicationController
   def new
-    @coutomer = Customer.new
+    @item = Item.new
   end
   def create
-
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_item(item.id)
   end
   def index
+    @items = Item.page(params[:page])
+  end
 
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
   def item_params
-    params.require(:item).permit(:genre_id, :name, :introduction, :price, :image)  end
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :image, :is_active)
+  end
 end
